9c9813cbfe3941a7e719dc853f4d69e7c0ae569c
nahid-dev <nahidv232@gmail.com>
2023-08-08 17:13:58 +0600

    header update done

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -18,6 +18,29 @@ const Header = () => {
   const navigate = useNavigate();
   const location = useLocation();
 
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [location]);
+
+  const controlNavbar = () => {
+    if (window.scrollY > 200) {
+      if (window.scrollY > lastScrollY && !mobileMenu) {
+        setShow("hide");
+      } else {
+        setShow("show");
+      }
+    } else {
+      setShow("top");
+    }
+    setLastScrollY(window.scrollY);
+  };
+  useEffect(() => {
+    window.addEventListener("scroll", controlNavbar);
+    return () => {
+      window.removeEventListener("scroll", controlNavbar);
+    };
+  }, [lastScrollY]);
+
   const openSearch = () => {
     setMobileMenu(false);
     setShowSearch(true);
@@ -37,6 +60,16 @@ const Header = () => {
     }
   };
 
+  const navigationHandler = (type) => {
+    if (type === "movie") {
+      navigate("/explore/movie");
+    } else {
+      navigate("/explore/tv");
+    }
+
+    setMobileMenu(false);
+  };
+
   return (
     <header className={`header ${mobileMenu ? "mobileView" : ""} ${show}`}>
       <ContentWrapper>
@@ -44,10 +77,14 @@ const Header = () => {
           <img src={logo} alt="" />
         </div>
         <ul className="menuItems">
-          <li className="menuItem">Movies</li>
-          <li className="menuItem">TV Shows</li>
+          <li className="menuItem" onClick={() => navigationHandler("movie")}>
+            Movies
+          </li>
+          <li className="menuItem" onClick={() => navigationHandler("tv")}>
+            TV Shows
+          </li>
           <li className="menuItem">
-            <HiOutlineSearch></HiOutlineSearch>
+            <HiOutlineSearch onClick={openSearch}></HiOutlineSearch>
           </li>
         </ul>
         <div className="mobileMenuItems">
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -6,6 +6,7 @@ const Home = () => {
   return (
     <div className="homePage">
       <HeroBanner></HeroBanner>
+      <div style={{ height: 1000 }}></div>
     </div>
   );
 };
