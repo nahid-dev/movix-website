a1c9358eeaf98ddf363a44893ed70f471d4451a6
nahid-dev <nahidv232@gmail.com>
2023-08-16 12:50:54 +0600

    update one

--- src/App.jsx
+++ src/App.jsx
@@ -41,8 +41,8 @@ function App() {
     });
     const data = await Promise.all(promises);
     // console.log(data);
-    data.map(({ genres }) => {
-      return genres.map((item) => (allGenres[item.id] = item));
+    data?.map(({ genres }) => {
+      return genres?.map((item) => (allGenres[item?.id] = item));
     });
     dispatch(getGenres(allGenres));
   };
